9f3832369a3fc0883e1d6d4c0659d16afb92e5ee
umayahara_k01 <umayahara_k01@A010117.pci-sol.local>
2013-07-03 17:27:42 +0900

    Android NDKのサンプルプロジェクト　native-plasma native codeで、ビットマップをnative activityで　プラズマ表示する

---
+++ ndk_sample_native_plasma/jni/plasma.hpp
@@ -0,0 +1,107 @@
+
+#include <jni.h>
+#include <errno.h>
+
+#include <sys/time.h>
+#include <time.h>
+#include <android/log.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <android_native_app_glue.h>
+#ifndef _Included_jp_umayahara_demo_check
+#define _Included_jp_umayahara_demo_check
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define  LOG_TAG    "libplasma"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+
+/* Set to 1 to enable debug log traces. */
+#define DEBUG 0
+
+/* Set to 1 to optimize memory stores when generating plasma. */
+#define OPTIMIZE_WRITES  1
+
+
+/* Return current time in milliseconds */
+static double now_ms(void)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec*1000. + tv.tv_usec/1000.;
+}
+
+/* We're going to perform computations for every pixel of the target
+ * bitmap. floating-point operations are very slow on ARMv5, and not
+ * too bad on ARMv7 with the exception of trigonometric functions.
+ *
+ * For better performance on all platforms, we're going to use fixed-point
+ * arithmetic and all kinds of tricks
+ */
+
+typedef int32_t  Fixed;
+
+#define  FIXED_BITS           16
+#define  FIXED_ONE            (1 << FIXED_BITS)
+#define  FIXED_AVERAGE(x,y)   (((x) + (y)) >> 1)
+
+#define  FIXED_FROM_INT(x)    ((x) << FIXED_BITS)
+#define  FIXED_TO_INT(x)      ((x) >> FIXED_BITS)
+
+#define  FIXED_FROM_FLOAT(x)  ((Fixed)((x)*FIXED_ONE))
+#define  FIXED_TO_FLOAT(x)    ((x)/(1.*FIXED_ONE))
+
+#define  FIXED_MUL(x,y)       (((int64_t)(x) * (y)) >> FIXED_BITS)
+#define  FIXED_DIV(x,y)       (((int64_t)(x) * FIXED_ONE) / (y))
+
+#define  FIXED_DIV2(x)        ((x) >> 1)
+#define  FIXED_AVERAGE(x,y)   (((x) + (y)) >> 1)
+
+#define  FIXED_FRAC(x)        ((x) & ((1 << FIXED_BITS)-1))
+#define  FIXED_TRUNC(x)       ((x) & ~((1 << FIXED_BITS)-1))
+
+#define  FIXED_FROM_INT_FLOAT(x,f)   (Fixed)((x)*(FIXED_ONE*(f)))
+
+typedef int32_t  Angle;
+
+#define  ANGLE_BITS              9
+
+#if ANGLE_BITS < 8
+#  error ANGLE_BITS must be at least 8
+#endif
+
+#define  ANGLE_2PI               (1 << ANGLE_BITS)
+#define  ANGLE_PI                (1 << (ANGLE_BITS-1))
+#define  ANGLE_PI2               (1 << (ANGLE_BITS-2))
+#define  ANGLE_PI4               (1 << (ANGLE_BITS-3))
+
+#define  ANGLE_FROM_FLOAT(x)   (Angle)((x)*ANGLE_PI/M_PI)
+#define  ANGLE_TO_FLOAT(x)     ((x)*M_PI/ANGLE_PI)
+
+#if ANGLE_BITS <= FIXED_BITS
+#  define  ANGLE_FROM_FIXED(x)     (Angle)((x) >> (FIXED_BITS - ANGLE_BITS))
+#  define  ANGLE_TO_FIXED(x)       (Fixed)((x) << (FIXED_BITS - ANGLE_BITS))
+#else
+#  define  ANGLE_FROM_FIXED(x)     (Angle)((x) << (ANGLE_BITS - FIXED_BITS))
+#  define  ANGLE_TO_FIXED(x)       (Fixed)((x) >> (ANGLE_BITS - FIXED_BITS))
+#endif
+
+
+static Fixed  angle_sin_tab[ANGLE_2PI+1];
+
+
+/* Color palette used for rendering the plasma */
+#define  PALETTE_BITS   8
+#define  PALETTE_SIZE   (1 << PALETTE_BITS)
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ ndk_sample_native_plasma/obj/local/armeabi-v7a/objs-debug/native-plasma/plasma.o.d
@@ -0,0 +1,184 @@
+obj/local/armeabi-v7a/objs-debug/native-plasma/plasma.o: jni/plasma.cpp \
+ jni/plasma.hpp \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/jni.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/errno.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/errno.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/errno.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdint.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/time.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/log.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdio.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/string.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/malloc.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/alloca.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/strings.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/memory.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/math.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/limits.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/sources/android/native_app_glue/android_native_app_glue.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/poll.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/poll.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/poll.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/pthread.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/signal.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/signal.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sched.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/configuration.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/asset_manager.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/looper.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/native_activity.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/input.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/keycodes.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/native_window.h \
+ C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/rect.h
+
+jni/plasma.hpp:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/jni.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/errno.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/errno.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/errno.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/time.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/log.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/string.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/strings.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/memory.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/math.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/limits.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/sources/android/native_app_glue/android_native_app_glue.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/poll.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/poll.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/poll.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/pthread.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/signal.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/signal.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sched.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/configuration.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/asset_manager.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/looper.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/native_activity.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/input.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/keycodes.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/native_window.h:
+
+C:/Users/umayahara_k01/Downloads/20130604/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/rect.h:
